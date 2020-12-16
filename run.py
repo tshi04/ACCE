dd9deb3051278f183ec550e4d60a321c007f39d9
tshi04 <tshi@vt.edu>
2020-12-16 22:48:38 +0000

    update

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
 ### Automatic Corpus-level and Concept Based Explanation for Document Classification
-from ACEclass.BertAttnCPTDrop.main import *
-from ACEclass.RNNAttnWECPTDrop.main import *
+from ACEclass.BertAttn.main import *
+# from ACEclass.BertAttnCPTDrop.main import *
+# from ACEclass.RNNAttnWE.main import *
+# from ACEclass.RNNAttnWECPTDrop.main import *
